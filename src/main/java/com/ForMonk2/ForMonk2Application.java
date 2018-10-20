a07fa66705c810c341a541bf14e54ffbed0845cf
tanmay_majumdar <tanmay.majumdar@nowfloats.com>
2018-10-20 21:11:13 +0530

    project migrated from maven to gradle

---
+++ src/main/java/com/ForMonk2/ForMonk2Application.java
@@ -0,0 +1,12 @@
+package com.ForMonk2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForMonk2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForMonk2Application.class, args);
+	}
+}
