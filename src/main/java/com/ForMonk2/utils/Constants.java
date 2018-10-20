8077709287f2fece1f70e9795d24be06804ad2ce
tanmay_majumdar <tanmay.majumdar@nowfloats.com>
2018-10-20 22:32:39 +0530

    db handler class created

--- src/main/java/com/ForMonk2/utils/Constants.java
+++ src/main/java/com/ForMonk2/utils/Constants.java
@@ -5,4 +5,13 @@ public class Constants {
 	public final static String DB_HOST = "localhost";
 	public final static int DB_PORT = 27017;
 
+	
+	public final static class DB_CREDENTIALS {
+		
+		public final static String 	USERNAME = "InfluenceMonk";
+		public final static String PASSWORD = "InfluenceMonk23235616";
+		public final static String DB_NAME = "MonkDB";
+		
+	}
+
 }
--- src/main/java/com/ForMonk2/utils/DBHandler.java
+++ src/main/java/com/ForMonk2/utils/DBHandler.java
@@ -2,24 +2,68 @@ package com.ForMonk2.utils;
 
 import java.net.UnknownHostException;
 
-import com.mongodb.MongoClient; 
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential; 
 
-public class DBHandler {
+public class DBHandler{
 	
 	
 	private MongoClient mongoClient;
+	private boolean connectionExists = false;
+	private DB monkDB;
+	
 	
 	protected DBHandler() {
 		try {
-			mongoClient = new MongoClient();
+			
+			if(! connectionExists) {
+				
+				mongoClient = new MongoClient(Constants.DB_HOST , Constants.DB_PORT);
+				
+				MongoCredential credential = MongoCredential.createMongoCRCredential(Constants.DB_CREDENTIALS.USERNAME,
+																 Constants.DB_CREDENTIALS.DB_NAME , 
+																 Constants.DB_CREDENTIALS.PASSWORD.toCharArray());
+				
+				printStackTrace("Connection created successfully");
+				
+				monkDB =  mongoClient.getDB(Constants.DB_CREDENTIALS.DB_NAME);
+				
+				connectionExists = true;
+				
+				
+			}
+			
+			
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
+			connectionExists = false;
 		}
 		
 	}
 	
 	
-	private boolean checkIfConnectionExists() {
-		return false;
+	private void printStackTrace(String message) {
+		
+		if(message != null ) {
+			if(message.trim() != "") {
+				System.out.println(message);
+			}
+		}
 	}
+	
+	private DB getMonkDB(){
+		
+		if(monkDB != null ) {
+			
+			return monkDB;
+			
+		}else {
+			
+			connectionExists = false;
+			
+			throw new RuntimeException("Sorry couldn't connect to "+Constants.DB_CREDENTIALS.DB_NAME);
+		}
+	}
+	
 }
