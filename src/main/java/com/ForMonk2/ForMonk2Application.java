cd9cc95b23f9214ee86e3b4d69dee2e8a5359cfb
tanmay_majumdar <tanmay.majumdar@nowfloats.com>
2019-01-08 02:59:03 +0530

    Configured swagger

--- src/main/java/com/ForMonk2/ForMonk2Application.java
+++ src/main/java/com/ForMonk2/ForMonk2Application.java
@@ -1,20 +1,55 @@
 package com.ForMonk2;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/*
+ * http://localhost:5000/docApi/swagger-ui.html
+ * */
 
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
-
-public class ForMonk2Application {
+@EnableSwagger2
+public class ForMonk2Application extends WebMvcConfigurationSupport{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForMonk2Application.class, args);
 	}
+	
+	@Bean
+	public Docket CachingTutorialsApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select().apis(RequestHandlerSelectors.basePackage("com.ForMonk2.controllers"))
+				.paths(regex("/*.*"))
+				.build();
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+	    registry.addRedirectViewController("/docApi/v2/api-docs", "/v2/api-docs");
+	    registry.addRedirectViewController("/docApi/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+	    registry.addRedirectViewController("/docApi/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+	    registry.addRedirectViewController("/docApi/swagger-resources", "/swagger-resources");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("/docApi/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+	    registry.addResourceHandler("/docApi/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+	
 	
 	  
 }
