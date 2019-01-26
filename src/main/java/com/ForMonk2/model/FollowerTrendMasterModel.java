3820c40464b1ac174d07d083c5482057246d5f4b
tanmay_majumdar <tanmay.majumdar@nowfloats.com>
2019-01-26 21:35:05 +0530

    Cron job completed

--- src/main/java/com/ForMonk2/model/FollowerTrendMasterModel.java
+++ src/main/java/com/ForMonk2/model/FollowerTrendMasterModel.java
@@ -2,72 +2,115 @@ package com.ForMonk2.model;
 
 import java.util.ArrayList;
 
+import org.bson.Document;
+import org.bson.codecs.pojo.annotations.BsonProperty;
 
 import com.ForMonk2.utils.CollectionHandler;
 import com.ForMonk2.utils.CollectionUtils;
+import com.ForMonk2.utils.CollectionUtils.DBCollections;
+import com.ForMonk2.utils.CollectionUtils.Operations;
+import com.google.gson.Gson;
 
 public class FollowerTrendMasterModel {
 
- private String _id;
- private String imcId;
- private ArrayList < FTMData > data = new ArrayList < FTMData > ();
+	@BsonProperty("_id")
+	private String _id;
 
+	@BsonProperty("imcId")
+	private String imcId;
 
- // Getter Methods 
+	@BsonProperty("data")
+	private ArrayList<FTMData> data;
 
- public String get_id() {
-  return _id;
- }
+	// Getter Methods
 
- public String getIMCId() {
-  return imcId;
- }
-
- // Setter Methods 
-
- public void set_id(String _id) {
-  this._id = _id;
- }
-
- public void setIMCId(String IMCId) {
-  this.imcId = IMCId;
- }
- 
- public ArrayList< FTMData > getData(){
-	 return data;
- }
- 
- public void setData(ArrayList<FTMData> data) {
-	 this.data = data;
- }
- 
- public static class FTMData{
-	 private long timestamp;
-	 
-	 public long getTimestamp() {
-		return timestamp;
+	public String get_id() {
+		return _id;
 	}
-	public void setTimestamp(long timestamp) {
-		this.timestamp = timestamp;
+
+	public String getIMCId() {
+		return imcId;
 	}
-	public long getFollowers() {
-		return followers;
+
+	// Setter Methods
+
+	public void set_id(String _id) {
+		this._id = _id;
 	}
-	public void setFollowers(long followers) {
-		this.followers = followers;
+
+	public void setIMCId(String IMCId) {
+		this.imcId = IMCId;
 	}
-	public long getMediaCount() {
-		return mediaCount;
+
+	public ArrayList<FTMData> getData() {
+		return data;
 	}
-	public void setMediaCount(long mediaCount) {
-		this.mediaCount = mediaCount;
+
+	public void setData(ArrayList<FTMData> data) {
+		this.data = data;
 	}
-	private long followers;
-	private long mediaCount;
- }
- 
- public void addToCollection() {
-	 CollectionHandler.startOperation(this, CollectionUtils.DBCollections.FollowerTrendMaster, CollectionUtils.Operations.create);
- }
+
+	public static class FTMData {
+		private long timestamp;
+
+		public long getTimestamp() {
+			return timestamp;
+		}
+
+		public void setTimestamp(long timestamp) {
+			this.timestamp = timestamp;
+		}
+
+		public long getFollowers() {
+			return followers;
+		}
+
+		public void setFollowers(long followers) {
+			this.followers = followers;
+		}
+
+		public long getMediaCount() {
+			return mediaCount;
+		}
+
+		public void setMediaCount(long mediaCount) {
+			this.mediaCount = mediaCount;
+		}
+
+		@BsonProperty("followers")
+		private long followers;
+
+		@BsonProperty("mediaCount")
+		private long mediaCount;
+	}
+
+	public void addToCollection() {
+		CollectionHandler.startOperation(this, CollectionUtils.DBCollections.FollowerTrendMaster,
+				CollectionUtils.Operations.create);
+	}
+
+	private Object getObjectIterable() {
+		return CollectionHandler.startOperation(new Gson().toJson(this), DBCollections.FollowerTrendMaster,
+				Operations.read);
+	}
+
+	@SuppressWarnings("unchecked")
+	public FollowerTrendMasterModel getDBObject() {
+		Document document = CollectionHandler.GetSingleData(getObjectIterable());
+		_id = document.getObjectId("_id").toString();
+		imcId = document.getString("imcId");
+		data = (ArrayList<FTMData>) document.get("data");
+
+		return this;
+
+	}
+
+	public void updateData(UpdateModel updateData) {
+		
+		
+		
+		CollectionHandler.startOperation(updateData, CollectionUtils.DBCollections.FollowerTrendMaster,
+				CollectionUtils.Operations.update);
+	}
 
 }
--- src/main/java/com/ForMonk2/utils/CollectionHandler.java
+++ src/main/java/com/ForMonk2/utils/CollectionHandler.java
@@ -2,6 +2,7 @@ package com.ForMonk2.utils;
 
 import org.bson.Document;
 import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
 
 import com.ForMonk2.model.UpdateModel;
 import com.ForMonk2.utils.CollectionUtils.DBCollections;
@@ -11,63 +12,61 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 
+public class CollectionHandler extends DBHandler {
 
-public class CollectionHandler extends DBHandler{
-	
-	private CollectionHandler() {} // this class is always supposed to be a singleton
-	
+	private CollectionHandler() {
+	} // this class is always supposed to be a singleton
+
 	private static CollectionHandler collectionHandler = null;
-	
+
 	/*
 	 * Method to handle CRUD operations for a collection
 	 */
 	public static Object startOperation(Object data, DBCollections collection, Operations operation) {
-		
+
 		Object response = false;
-		
+
 		try {
-			
-			if(collectionHandler == null) {
+
+			if (collectionHandler == null) {
 				collectionHandler = new CollectionHandler();
 			}
-			
-			System.out.println("Collection: "+collection.toString()+" Operation: "+operation.toString());
-			
-			MongoCollection<Document> mongoCollection = getMonkDB().getCollection(collection.toString()); 
-		
+
+			System.out.println("Collection: " + collection.toString() + " Operation: " + operation.toString());
+
+			MongoCollection<Document> mongoCollection = getMonkDB().getCollection(collection.toString());
+
 			switch (operation) {
-			
-				case create: 
-					create(data, mongoCollection);
-					response = true;
-					break;
-				
-				case update:
-					update((UpdateModel) data, mongoCollection);
-					response = true;
-					break;
-				
-				case delete:
-					delete((String) data, mongoCollection);
-					response = true;
-					break;
-					
-				case read:
-					response = read((String) data, mongoCollection);
-					break;
-				
+
+			case create:
+				create(data, mongoCollection);
+				response = true;
+				break;
+
+			case update:
+				update((UpdateModel) data, mongoCollection);
+				response = true;
+				break;
+
+			case delete:
+				delete((String) data, mongoCollection);
+				response = true;
+				break;
+
+			case read:
+				response = read((String) data, mongoCollection);
+				break;
+
 			}
-		
-		}
-		catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return response;
-		
+
 	}
-	
-	
+
 	/*
 	 * Method to add a document in a collection
 	 */
@@ -77,64 +76,63 @@ public class CollectionHandler extends DBHandler{
 		Document document = Document.parse(jsonData);
 		mongoCollection.insertOne(document);
 	}
-	
+
 	/*
 	 * Method to update documents in a collection
 	 */
 	private static void update(UpdateModel updateModel, MongoCollection<Document> mongoCollection) {
 		BasicDBObject updatedData = new BasicDBObject();
-		
-		for(String key: updateModel.getUpdateMap().keySet()) {
-			updatedData.append(key, updateModel.getUpdateMap().get(key));
-		}	
-		
+
+		for (String key : updateModel.getUpdateMap().keySet()) {
+			if (key != "_id")
+				updatedData.append(key, updateModel.getUpdateMap().get(key));
+			
+			//updatedData.append(key, updateModel.getUpdateMap().get(key));
+
+		}
+
 		BasicDBObject updatedDocument = new BasicDBObject();
 		updatedDocument.append("$set", updatedData);
-	
-		Bson queryBson =  BasicDBObject.parse(updateModel.getQuery());
-		
+
+		Bson queryBson = BasicDBObject.parse(updateModel.getQuery());
+
 		mongoCollection.updateMany(queryBson, updatedDocument);
-		
+
 	}
-	
+
 	/*
 	 * Method to delete documents in a collection
 	 */
 	private static void delete(String queryString, MongoCollection<Document> mongoCollection) {
-		Bson filter =  BasicDBObject.parse(queryString);
+		Bson filter = BasicDBObject.parse(queryString);
 		mongoCollection.deleteMany(filter);
 	}
-	
-	
+
 	/*
 	 * Method to query a collection
 	 */
 	private static FindIterable<Document> read(String queryString, MongoCollection<Document> mongoCollection) {
-		Bson bson =  BasicDBObject.parse(queryString);
+		Bson bson = BasicDBObject.parse(queryString);
 		return mongoCollection.find(bson);
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public static <T> T GetSingleData(Object mainObjectIterator) {
-		
-		if(! (mainObjectIterator instanceof FindIterable)) {
+	public static Document GetSingleData(Object mainObjectIterator) {
+
+		if (!(mainObjectIterator instanceof FindIterable)) {
 			throw new RuntimeException("Please provide a iterable");
 		}
-		
-		FindIterable<Document> mainObject = (FindIterable<Document>)mainObjectIterator;
-		
-		if(mainObject != null && mainObject.iterator().hasNext()) {
-			return (T)(new Gson().fromJson(mainObject.first().toJson() , Object.class));
-		}else if(!mainObject.iterator().hasNext()) {
-			return null; 
-		}
-		else {
+
+		FindIterable<Document> mainObject = (FindIterable<Document>) mainObjectIterator;
+
+		if (mainObject != null && mainObject.iterator().hasNext()) {
+			return mainObject.iterator().next();
+		} else if (!mainObject.iterator().hasNext()) {
+			return null;
+		} else {
 			throw new RuntimeException("Can't convert null BSON ");
 		}
-		
+
 	}
-	
-	
-	
 
 }
