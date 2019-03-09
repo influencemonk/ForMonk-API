c6057926ec93ca0ec33177586ce28d5b79f9689a
Soumya Deb <soumyadeb1809@gmail.com>
2019-03-10 04:17:48 +0530

    Added profile link dto.

---
+++ src/main/java/com/ForMonk2/dto/ProfileLinkDto.java
@@ -0,0 +1,46 @@
+package com.ForMonk2.dto;
+
+public class ProfileLinkDto {
+	
+	private String imcId;
+    private String linkUrl;
+    private String postImageUrl;
+    private String linkTitle;
+    private String clientId;
+    
+    
+	public String getImcId() {
+		return imcId;
+	}
+	public void setImcId(String imcId) {
+		this.imcId = imcId;
+	}
+	public String getLinkUrl() {
+		return linkUrl;
+	}
+	public void setLinkUrl(String linkUrl) {
+		this.linkUrl = linkUrl;
+	}
+	public String getPostImageUrl() {
+		return postImageUrl;
+	}
+	public void setPostImageUrl(String postImageUrl) {
+		this.postImageUrl = postImageUrl;
+	}
+	public String getLinkTitle() {
+		return linkTitle;
+	}
+	public void setLinkTitle(String linkTitle) {
+		this.linkTitle = linkTitle;
+	}
+	public String getClientId() {
+		return clientId;
+	}
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+    
+    
+    
+
+}
